b1e73ae4d5409c740061a8fb656756f226b1ee08
Pirama Arumuga Nainar <pirama@google.com>
2025-07-22 22:58:51 +0000

    Update prebuilt Clang to r563880b (21.0.0).
    clang 21.0.0 (based on r563880b) from build 13818152.
    Test: N/A
    
    Change-Id: I32f57cb61def5ffdbf101ae53133022ffcb5c02e

---
+++ clang-r563880b/include/llvm/Support/VCSRevision.h
@@ -0,0 +1,2 @@
+#define LLVM_REVISION "5e96669f06077099aa41290cdb4c5e6fa0f59349"
+#define LLVM_REPOSITORY "/Volumes/Android/buildbot/src/googleplex-android/llvm-r563880-release/out/llvm-project/llvm"
